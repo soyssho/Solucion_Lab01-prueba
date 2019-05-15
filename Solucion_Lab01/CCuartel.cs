0d59a9bb0091db6fb39fbc6457e101525e756ed0
soyssho <50681997+soyssho@users.noreply.github.com>
2019-05-15 17:37:20 -0500

    Update CCuartel.cs

--- Solucion_Lab01/CCuartel.cs
+++ Solucion_Lab01/CCuartel.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using System;f this project for you to commit your proposed changes to. Submitting a change to this file will write it to a new branch in your fork, so you
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks  ;
 
 namespace Solucion_Lab01
 {
     public class CCuartel
-    {
+    {public string gg{get;set;}
         public string Codigo { get; set; }
         public string NombreCuartel { get; set; }
         public string Direccion { get; set; }
